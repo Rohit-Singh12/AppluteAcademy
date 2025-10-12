f17bc0cfc27a3117534e654a00763394083424d3
Rohit-Singh12 <2022singhrohit@gmail.com>
2025-10-12 14:11:35 +0530

    allowed hosts changes

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     host: '0.0.0.0',  // bind to all interfaces
     port: 5173,
     allowedHosts: [
-      'applute.com',
+      'academy.applute.com',
     ]
   }
 });
